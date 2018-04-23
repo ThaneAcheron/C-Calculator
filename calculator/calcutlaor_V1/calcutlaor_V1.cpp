f2de7d8c3d0af027389a5a19374504d2e422db11
ThaneAcheron <craig.turner944@gmail.com>
2018-04-23 09:51:18 +0200

    Final

---
+++ calculator/calcutlaor_V1/calcutlaor_V1.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+
+int main() 
+ { 
+           using namespace std; 
+           
+           string menuanswer;
+           
+           cout << ("type help for menu");
+           cin >> menuanswer; 
+           
+           if menuanswer = "help"  ; 
+           
+           cout << "pluss"; 
+      
+           
+           cin >> menu_answer; 
+           
+           
+          
+           system ("puase");  
+           return 0; 
+            
+           } 
+ 
+        
+          
